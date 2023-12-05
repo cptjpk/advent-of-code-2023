1b2419f51fac6858da21fcf42e50a8e51e6c1116
Jeff P <jpidcock@me.com>
2023-12-05 16:50:05 +0000

    Refactor file reading and sum calculation in main.rs

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,12 +1,43 @@
-use std::fs;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::collections::HashMap;
 
 fn main() {
-    println!("Hello, world!");
+
+    let file_name: &str = "../day01/day01-input.txt";
+    let file: File = File::open(file_name).expect("file not found");
+    let mut sum: i32;
+
+    let mut reader: BufReader<File> = std::io::BufReader::new(file);
+
+    sum = part_one(&mut reader);
+    println!("Cal Value (digits only): {sum}");
+}
+
+fn part_one(reader: &mut BufReader<File>) -> i32{ 
+
+    let mut sum: i32 = 0;
+    for (_index, line) in reader.lines().enumerate() {
+        let line: String = line.expect("error reading line");
+        let mut first: i32 = -1;
+        let mut second: i32 = -1;
+        let n: i32;
+        for i in line.chars() {
+            if i.is_numeric() {
+                if first == -1 {
+                    first = i.to_digit(10).unwrap() as i32;
+                }
+                else{
+                    second = i.to_digit(10).unwrap() as i32;
+                }
+            }
+        }
+        if second == -1 {
+            second = first;
+        }
+        n = first *  10 + second;
+        sum += n;
+    }
     
-    let cal = fs::read_to_string("day01-input.txt")
-                                .expect("Tried to read day01-input.txt, failed.");
-
-    let lines = cal.lines().map(String::from).collect();
-
-    println!("{cal}");
+    sum
 }

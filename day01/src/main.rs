1810c6e61b5214662ad2b797d2094d06dbe303e1
cptjpk <77907794+cptjpk@users.noreply.github.com>
2023-12-04 23:45:44 -0500

    Update main.rs
    Typo.

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -3,7 +3,7 @@ use std::fs;
 fn main() {
     println!("Hello, world!");
     
-    let calibration cal = fs::read_to_string("day01-input.txt")
+    let cal = fs::read_to_string("day01-input.txt")
                                 .expect("Tried to read day01-input.txt, failed.");
                                 
     println!("{cal}");

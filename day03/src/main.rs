a45ce08bee26c4d3bfbae2a2a8a82260958ea24f
Jeff P <jpidcock@me.com>
2023-12-08 20:07:29 +0000

    Finalize solution for Part 2.

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -9,14 +9,34 @@ struct Number {
     length: i32,
     found: bool,
 }
+
+struct Symbol {
+    value: char,
+    x: i32,
+    y: i32,
+    adj: i32,
+}
+
 fn main() {
-    let mut file = File::open("./src/sample.txt").expect("Failed to open file");
+    let mut file = File::open("./input.txt").expect("Failed to open file");
     let mut contents = String::new();
     file.read_to_string(&mut contents).expect("Failed to read file");
+
     let mut numbers: Vec<Number> = Vec::new();
-    let mut sum = 0;
+    let mut sum: u32;
 
     let lines: Vec<&str> = contents.split('\n').collect();
+
+    sum = part_one(&lines, &mut numbers);
+    println!("Part One Sum: {}", sum);
+
+    sum = part_two(&lines, &mut numbers);
+    println!("Part Two Sum: {}", sum);
+}
+
+fn part_one(lines: &Vec<&str>, numbers: &mut Vec<Number>) -> u32{
+
+    let mut sum: u32 = 0;
 
     for (y, line) in lines.iter().enumerate() {
         // TODO: Process each line here
@@ -28,51 +48,133 @@ fn main() {
                 }
                 else{
                     t_num = t_num * 10 + c.to_digit(10).unwrap() as i32;
+                    if x == line.len() - 1 {
+                        numbers.push(Number {
+                            value: t_num,
+                            x: (x as i32 - t_num.to_string().len() as i32),
+                            y: y as i32,
+                            length: t_num.to_string().len() as i32,
+                            found: false,
+                        });
+                        t_num = -1;
+                    }
                 }
                 continue;
             }
-            else if !c.is_numeric() && t_num != -1 {
+            else if !c.is_numeric() && t_num != -1{
                 numbers.push(Number {
                     value: t_num,
-                    x: x as i32,
+                    x: (x as i32 - t_num.to_string().len() as i32),
                     y: y as i32,
                     length: t_num.to_string().len() as i32,
                     found: false,
                 });
                 t_num = -1;
             }
+            
         }
     }
 
     for (y, line) in lines.iter().enumerate() {
         for (x, c) in line.chars().enumerate() {
             if c != '.' && !c.is_numeric() {
-                println!("Symbol '{}' found at ({}, {})", c, x, y);
-                for dx in 0..2{
-                    for dy in 0..2{
+                for dx in 0..3{
+                    for dy in 0..3{
                         let cx = x + dx - 1;
                         let cy = y + dy - 1;
-                        println!("Checking ({}, {})", cx, cy);  
-                        
+
                         for num in numbers.iter_mut(){
                             if num.y == cy as i32{
                                 let pos_x = num.x .. num.x + num.length as i32;
-                                // println!("{}: {:?}", num.value, pos_x);
                                 if pos_x.contains(&(cx as i32)) && !num.found{
-                                    println!("Found {}", num.value);
                                     num.found = true;
-                                    sum += num.value;
-                                    continue;
+                                    sum += num.value as u32;
                                 }
                             }
                         }
                     }
                 }
-                println!("-----------------");
             }
         }
     }
-    
 
-    println!("Sum: {}", sum);
+    sum
+}
+
+fn part_two(lines: &Vec<&str>, numbers: &mut Vec<Number>) -> u32{
+
+    let mut sum: u32 = 0;
+
+    for (y, line) in lines.iter().enumerate() {
+        // TODO: Process each line here
+        let mut t_num = -1;
+        for (x, c) in line.chars().enumerate() {
+            if c.is_numeric() {
+                if t_num == -1 {
+                    t_num = c.to_digit(10).unwrap() as i32;
+                }
+                else{
+                    t_num = t_num * 10 + c.to_digit(10).unwrap() as i32;
+                    if x == line.len() - 1 {
+                        numbers.push(Number {
+                            value: t_num,
+                            x: (x as i32 - t_num.to_string().len() as i32),
+                            y: y as i32,
+                            length: t_num.to_string().len() as i32,
+                            found: false,
+                        });
+                        t_num = -1;
+                    }
+                }
+                continue;
+            }
+            else if !c.is_numeric() && t_num != -1{
+                numbers.push(Number {
+                    value: t_num,
+                    x: (x as i32 - t_num.to_string().len() as i32),
+                    y: y as i32,
+                    length: t_num.to_string().len() as i32,
+                    found: false,
+                });
+                t_num = -1;
+            }
+            
+        }
+    }
+
+    for (y, line) in lines.iter().enumerate() {
+        for (x, c) in line.chars().enumerate() {
+            if c == '*' {
+                let mut adj = 0;
+                let mut adj_s = 0;
+                for dx in 0..3{
+                    for dy in 0..3{
+                        let cx = x + dx - 1;
+                        let cy = y + dy - 1;
+
+                        for num in numbers.iter_mut(){
+                            if num.y == cy as i32 && adj < 3{
+                                let pos_x = num.x .. num.x + num.length as i32;
+                                if pos_x.contains(&(cx as i32)) && !num.found{
+                                    num.found = true;
+                                    if adj == 0{
+                                        adj_s = num.value;
+                                    }
+                                    else{
+                                        adj_s *= num.value;
+                                    }
+                                    adj += 1;
+                                }
+                            }
+                        }
+                    }
+                }
+                if adj == 2{
+                    sum += adj_s as u32;
+                }
+            }
+        }
+    }
+
+    sum
 }

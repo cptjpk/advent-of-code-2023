9d2fdf95424dfe56c5dfce84b1645ef68b0a47c7
Jeff P <jpidcock@me.com>
2023-12-08 20:31:44 +0000

    Staging Day 4

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,3 +1,9 @@
+use std::fs::File;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    let mut file = File::open("./sample.txt").expect("Failed to open file");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("Failed to read file");
+    
 }

73293986ba65fe48ee2ae1cae5b65b5684317833
Jeff P <jpidcock@me.com>
2023-12-08 21:11:33 +0000

    Implement logic for comparing winners.

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,9 +1,35 @@
 use std::fs::File;
 use std::io::Read;
+use std::collections::HashSet;
 
 fn main() {
     let mut file = File::open("./sample.txt").expect("Failed to open file");
     let mut contents = String::new();
     file.read_to_string(&mut contents).expect("Failed to read file");
+    let lines: Vec<&str> = contents.split('\n').collect();
+
+    for (idx, line) in lines.iter().enumerate() {
+        let card: Vec<&str> = line.split(": ").collect();
+        let numbers: Vec<&str> = card[1].split(" | ").collect::<Vec<&str>>();
+        let mut wnums: Vec<i32> = Vec::new();
+        let mut pnums: Vec<i32> = Vec::new();
+        wnums.extend(numbers[0].split_whitespace()
+            .filter_map(|num_str| num_str.parse::<i32>().ok()));
+        pnums.extend(numbers[1].split_whitespace()
+            .filter_map(|num_str| num_str.parse::<i32>().ok()));
+        println!("Game {}: Winning Numbers: {:?}, Picked Numbers: {:?}", idx, wnums, pnums);
+        println!("Score: {}: ", check_winners(&wnums, &pnums));
+        println!("---------------------------");
+    }
     
+}
+
+fn check_winners(winning_numbers: &Vec<i32>, picked_numbers: &Vec<i32>) -> i32{
+    let wn: HashSet<_> = winning_numbers.into_iter().collect();
+    let pn: HashSet<_> = picked_numbers.into_iter().collect();
+
+    let common_numbers: HashSet<_> = pn.intersection(&wn).collect();
+    println!("Numbers that Hit: {:?}", common_numbers);
+
+    common_numbers.len() as i32 * 2
 }

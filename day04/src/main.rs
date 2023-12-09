b9e31d2cda8d9b5aa17d72f566d8bbd68b7e68b1
Jeff P <jpidcock@me.com>
2023-12-09 17:28:29 +0000

    Part One Complete

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -3,12 +3,14 @@ use std::io::Read;
 use std::collections::HashSet;
 
 fn main() {
-    let mut file = File::open("./sample.txt").expect("Failed to open file");
+    let mut file = File::open("./input.txt").expect("Failed to open file");
     let mut contents = String::new();
     file.read_to_string(&mut contents).expect("Failed to read file");
     let lines: Vec<&str> = contents.split('\n').collect();
 
-    for (idx, line) in lines.iter().enumerate() {
+    let mut running_score: i32 = 0;
+
+    for (_, line) in lines.iter().enumerate() {
         let card: Vec<&str> = line.split(": ").collect();
         let numbers: Vec<&str> = card[1].split(" | ").collect::<Vec<&str>>();
         let mut wnums: Vec<i32> = Vec::new();
@@ -17,10 +19,10 @@ fn main() {
             .filter_map(|num_str| num_str.parse::<i32>().ok()));
         pnums.extend(numbers[1].split_whitespace()
             .filter_map(|num_str| num_str.parse::<i32>().ok()));
-        println!("Game {}: Winning Numbers: {:?}, Picked Numbers: {:?}", idx, wnums, pnums);
-        println!("Score: {}: ", check_winners(&wnums, &pnums));
-        println!("---------------------------");
+        running_score += check_winners(&wnums, &pnums);
     }
+
+    println!("Final Score: {}", running_score);
     
 }
 
@@ -29,7 +31,11 @@ fn check_winners(winning_numbers: &Vec<i32>, picked_numbers: &Vec<i32>) -> i32{
     let pn: HashSet<_> = picked_numbers.into_iter().collect();
 
     let common_numbers: HashSet<_> = pn.intersection(&wn).collect();
-    println!("Numbers that Hit: {:?}", common_numbers);
-
-    common_numbers.len() as i32 * 2
+    
+    if common_numbers.len() == 0{
+        0
+    }
+    else{
+        2_i32.pow(common_numbers.len() as u32 - 1)
+    }
 }
